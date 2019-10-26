6aa022e8d49da0f227a4477e4d9de953100359a1
Tepp <Madis.Tepp@sas.se>
2019-10-26 09:31:30 +0200

    not a number check added

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -3,5 +3,11 @@ describe('calculator', function() {
         it('should return 42', function() {
             expect(addition(20, 22)).toBe(42);
         })
+        it('should return 19', function() {
+            expect(addition(7, 12)).toBe(19);
+        })
+        it('should return an error if we dont supply two numbers', function() {
+            expect(addition('hitchhikers', 'guide')).toBe('Error!');
+        })
     })
 })
